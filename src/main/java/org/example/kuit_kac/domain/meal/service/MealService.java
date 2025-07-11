89db9d82ae0493eaa127146b1cc3bdad5eb15ba7
dlstkdals123 <dlstkdals123@naver.com>
2025-07-12 03:05:00 +0900

    [Feat] #16 - service 추가

---
+++ src/main/java/org/example/kuit_kac/domain/meal/service/MealService.java
@@ -0,0 +1,9 @@
+package org.example.kuit_kac.domain.meal.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MealService {
+}
