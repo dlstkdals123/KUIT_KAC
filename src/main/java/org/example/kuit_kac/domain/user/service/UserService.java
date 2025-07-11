99f885cd26ee2f10e65317a765bce3e2372049fe
dlstkdals123 <dlstkdals123@naver.com>
2025-07-12 03:05:00 +0900

    [Feat] #16 - service 추가

--- src/main/java/org/example/kuit_kac/domain/user/service/UserService.java
+++ src/main/java/org/example/kuit_kac/domain/user/service/UserService.java
@@ -8,6 +8,9 @@ import org.example.kuit_kac.exception.CustomException;
 import org.example.kuit_kac.exception.ErrorCode;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -17,15 +20,16 @@ public class UserService {
     public UserResponse getUserById(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        return UserResponse.from(user);
+    }
 
-        return new UserResponse(
-                user.getId(),
-                user.getNickname(),
-                user.getEmail(),
-                user.getGender(),
-                user.getAge(),
-                user.getHeight(),
-                user.getTarget_weight()
-        );
+    public List<UserResponse> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        if (users.isEmpty()) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        }
+        return users.stream()
+                .map(UserResponse::from)
+                .collect(Collectors.toList());
     }
 }
