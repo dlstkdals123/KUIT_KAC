8a79a125f49936262abd045b2050c213a5232ade
dlstkdals123 <dlstkdals123@naver.com>
2025-07-13 02:27:23 +0900

    [Refactor] #5 - Controller의 Parameter 어노테이션 추가

--- src/main/java/org/example/kuit_kac/domain/user/controller/UserController.java
+++ src/main/java/org/example/kuit_kac/domain/user/controller/UserController.java
@@ -1,10 +1,12 @@
 package org.example.kuit_kac.domain.user.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.kuit_kac.domain.user.dto.UserResponse;
 import org.example.kuit_kac.domain.user.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,7 +19,11 @@ public class UserController {
 
     @GetMapping("/{id}")
     @Operation(summary = "사용자 ID로 단일 사용자 정보 조회", description = "제공된 사용자 ID를 사용하여 특정 사용자의 상세 프로필 정보를 조회합니다.")
-    public UserResponse getUser(@PathVariable Long id) {
-        return userService.getUserById(id);
+    public ResponseEntity<UserResponse> getUser(
+            @Parameter(description = "조회할 사용자의 고유 ID", example = "1")
+            @PathVariable Long id
+    ) {
+        UserResponse userResponse = userService.getUserById(id);
+        return ResponseEntity.ok(userResponse);
     }
 }
