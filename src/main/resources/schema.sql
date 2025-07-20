450bab9eb22488d45a216d2aefa016427a5e9c77
dlstkdals123 <dlstkdals123@naver.com>
2025-07-20 13:35:38 +0900

    [Feat] #35 - ERD 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,9 @@
 -- 테이블 삭제 (외래 키 제약 조건 역순)
-DROP TABLE IF EXISTS `meal_aifood`;
-DROP TABLE IF EXISTS `meal_food`;
-DROP TABLE IF EXISTS `meal`;
+DROP TABLE IF EXISTS `diet_aifood`;
+DROP TABLE IF EXISTS `diet_food`;
 DROP TABLE IF EXISTS `diet`;
-DROP TABLE IF EXISTS `exercise_detail`;
-DROP TABLE IF EXISTS `exercise_record`;
 DROP TABLE IF EXISTS `weight`;
-DROP TABLE IF EXISTS `exercise_routine_information`;
-DROP TABLE IF EXISTS `exercise_routine`;
 DROP TABLE IF EXISTS `user_information`;
-DROP TABLE IF EXISTS `set_detail`;
-DROP TABLE IF EXISTS `exercise`;
 DROP TABLE IF EXISTS `aifood`;
 DROP TABLE IF EXISTS `food`;
 DROP TABLE IF EXISTS `user`;
@@ -53,27 +46,15 @@ CREATE TABLE `weight` (
 
 -- 음식 관련 테이블
 CREATE TABLE `diet` (
-    `id`               bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `user_id`          bigint                NOT NULL,
-    `diet_type`        ENUM('RECORD', 'PLAN', 'AI_PLAN', 'FASTING', 'DINING_OUT', 'DRINKING') NOT NULL,
-    `diet_date`        date                  NOT NULL,
-    `created_at`       datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`       datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE,
-    UNIQUE (`user_id`, `diet_type`, `diet_date`)
-);
-
-CREATE TABLE `meal` (
     `id`           bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `user_id`      bigint                NOT NULL,
-    `diet_id`      bigint                NULL,
     `name`         varchar(30)           NOT NULL,
-    `meal_type`    ENUM('BREAKFAST', 'LUNCH', 'DINNER', 'SNACK', 'TEMPLATE') NOT NULL,
-    `meal_time`    datetime              NULL,
+    `diet_type`    ENUM('BREAKFAST', 'LUNCH', 'DINNER', 'SNACK', 'FASTING', 'TEMPLATE') NOT NULL,
+    `diet_entry_type` ENUM('RECORD', 'PLAN', 'AI_PLAN', 'DINING_OUT', 'DRINKING') NOT NULL,
+    `diet_time`    datetime              NOT NULL,
     `created_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`diet_id`) REFERENCES `diet`(`id`) ON DELETE CASCADE
+    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `food` (
@@ -81,13 +62,17 @@ CREATE TABLE `food` (
     `name`           varchar(50)           NOT NULL UNIQUE,
     `unit_type`      varchar(20)           NOT NULL,
     `unit_num`       bigint                NOT NULL,
-    `food_type`      varchar(20)           NOT NULL,
+    `food_type`      ENUM('NORMAL_GRAIN_AND_TUBER', 'NORMAL_FRUIT', 'NORMAL_GRILLED', 'NORMAL_SOUP_AND_TANG', 'NORMAL_KIMCHI', 'NORMAL_NAMUL_AND_SUKCHAE', 'NORMAL_BEANS_AND_NUTS', 'NORMAL_NOODLE_AND_DUMPLING', 'NORMAL_RICE', 'NORMAL_STIR_FRIED', 'NORMAL_BREAD_AND_SNACK', 'NORMAL_FRESH_AND_MUCHIM', 'NORMAL_FISH_AND_MEAT', 'NORMAL_DAIRY_AND_ICECREAM', 'NORMAL_BEVERAGE_AND_TEA', 'NORMAL_SAUCE_AND_SEASONING', 'NORMAL_PICKLE', 'NORMAL_PANCAKE', 'NORMAL_SALTED_SEAFOOD', 'NORMAL_BRAISED', 'NORMAL_PORRIDGE_AND_SOUP', 'NORMAL_STEW_AND_HOT_POT', 'NORMAL_STEAMED', 'NORMAL_VEGETABLE_AND_SEAWEED', 'NORMAL_FRIED', 'PROCESSED_BREAD_AND_SNACK', 'PROCESSED_OTHER_FOOD', 'PROCESSED_AGRICULTURAL', 'PROCESSED_SUGAR', 'PROCESSED_ANIMAL', 'PROCESSED_TOFU_AND_MUK', 'PROCESSED_NOODLE', 'PROCESSED_ICECREAM', 'PROCESSED_SEAFOOD', 'PROCESSED_OIL', 'PROCESSED_MEAT', 'PROCESSED_EGG', 'PROCESSED_DAIRY', 'PROCESSED_BEVERAGE', 'PROCESSED_SAUCE', 'PROCESSED_JAM', 'PROCESSED_PICKLE_AND_BRAISED', 'PROCESSED_SEASONING', 'PROCESSED_ALCOHOL', 'PROCESSED_INSTANT', 'PROCESSED_COCOA_CHOCOLATE', 'PROCESSED_NUTRITION', 'PROCESSED_MEDICAL') NOT NULL,
     `is_processed_food` boolean            NOT NULL DEFAULT FALSE,
     `calorie`        double                NOT NULL DEFAULT 0.0,
     `carbohydrate`   double                NOT NULL DEFAULT 0.0,
+    `is_high_carbonhydrate` boolean        NOT NULL DEFAULT FALSE,
     `protein`        double                NOT NULL DEFAULT 0.0,
+    `is_high_protein` boolean              NOT NULL DEFAULT FALSE,
     `fat`            double                NOT NULL DEFAULT 0.0,
+    `is_high_fat`    boolean               NOT NULL DEFAULT FALSE,
     `sugar`          double                NOT NULL DEFAULT 0.0,
+    `score`          int                   NOT NULL,
     `created_at`     datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`     datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -98,103 +83,40 @@ CREATE TABLE `aifood` (
     `name`           varchar(50)           NOT NULL,
     `unit_type`      varchar(20)           NOT NULL,
     `unit_num`       bigint                NOT NULL,
-    `food_type`      varchar(20)           NOT NULL,
+    `food_type`      ENUM('NORMAL_GRAIN_AND_TUBER', 'NORMAL_FRUIT', 'NORMAL_GRILLED', 'NORMAL_SOUP_AND_TANG', 'NORMAL_KIMCHI', 'NORMAL_NAMUL_AND_SUKCHAE', 'NORMAL_BEANS_AND_NUTS', 'NORMAL_NOODLE_AND_DUMPLING', 'NORMAL_RICE', 'NORMAL_STIR_FRIED', 'NORMAL_BREAD_AND_SNACK', 'NORMAL_FRESH_AND_MUCHIM', 'NORMAL_FISH_AND_MEAT', 'NORMAL_DAIRY_AND_ICECREAM', 'NORMAL_BEVERAGE_AND_TEA', 'NORMAL_SAUCE_AND_SEASONING', 'NORMAL_PICKLE', 'NORMAL_PANCAKE', 'NORMAL_SALTED_SEAFOOD', 'NORMAL_BRAISED', 'NORMAL_PORRIDGE_AND_SOUP', 'NORMAL_STEW_AND_HOT_POT', 'NORMAL_STEAMED', 'NORMAL_VEGETABLE_AND_SEAWEED', 'NORMAL_FRIED', 'PROCESSED_BREAD_AND_SNACK', 'PROCESSED_OTHER_FOOD', 'PROCESSED_AGRICULTURAL', 'PROCESSED_SUGAR', 'PROCESSED_ANIMAL', 'PROCESSED_TOFU_AND_MUK', 'PROCESSED_NOODLE', 'PROCESSED_ICECREAM', 'PROCESSED_SEAFOOD', 'PROCESSED_OIL', 'PROCESSED_MEAT', 'PROCESSED_EGG', 'PROCESSED_DAIRY', 'PROCESSED_BEVERAGE', 'PROCESSED_SAUCE', 'PROCESSED_JAM', 'PROCESSED_PICKLE_AND_BRAISED', 'PROCESSED_SEASONING', 'PROCESSED_ALCOHOL', 'PROCESSED_INSTANT', 'PROCESSED_COCOA_CHOCOLATE', 'PROCESSED_NUTRITION', 'PROCESSED_MEDICAL') NOT NULL,
     `is_processed_food` boolean            NOT NULL DEFAULT FALSE,
     `calorie`        double                NOT NULL DEFAULT 0.0,
     `carbohydrate`   double                NOT NULL DEFAULT 0.0,
+    `is_high_carbonhydrate` boolean        NOT NULL DEFAULT FALSE,
     `protein`        double                NOT NULL DEFAULT 0.0,
+    `is_high_protein` boolean              NOT NULL DEFAULT FALSE,
     `fat`            double                NOT NULL DEFAULT 0.0,
+    `is_high_fat`    boolean               NOT NULL DEFAULT FALSE,
     `sugar`          double                NOT NULL DEFAULT 0.0,
+    `score`          int                   NOT NULL,
     `created_at`     datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`     datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE `meal_food` (
+CREATE TABLE `diet_food` (
     `id`           bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `meal_id`      bigint                NOT NULL,
+    `diet_id`      bigint                NOT NULL,
     `food_id`      bigint                NOT NULL,
     `quantity`     double                NOT NULL,
     `created_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`meal_id`) REFERENCES `meal`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`diet_id`) REFERENCES `diet`(`id`) ON DELETE CASCADE,
     FOREIGN KEY (`food_id`) REFERENCES `food`(`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE `meal_aifood` (
+CREATE TABLE `diet_aifood` (
     `id`           bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `meal_id`      bigint                NOT NULL,
+    `diet_id`      bigint                NOT NULL,
     `aifood_id`    bigint                NOT NULL,
     `quantity`     double                NOT NULL,
     `created_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`meal_id`) REFERENCES `meal`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`diet_id`) REFERENCES `diet`(`id`) ON DELETE CASCADE,
     FOREIGN KEY (`aifood_id`) REFERENCES `aifood`(`id`) ON DELETE CASCADE
-);
-
-
--- 운동 관련 테이블 (수정 필요)
-CREATE TABLE `exercise` (
-    `id`                  bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `name`                varchar(40)           NULL,
-    `target_muscle_group` varchar(20)           NOT NULL,
-    `met_value`           double                NULL,
-    `created_at`          datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`          datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE `set_detail` (
-    `id`           bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `count`        int                   NULL DEFAULT 0,
-    `weight_kg`    int                   NULL DEFAULT 0,
-    `weight_num`   int                   NULL DEFAULT 0,
-    `distance`     int                   NULL DEFAULT 0,
-    `time`         double                NULL DEFAULT 0,
-    `order`        int                   NULL DEFAULT 0,
-    `created_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE `exercise_record` (
-    `id`            bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `user_id`       bigint                NOT NULL,
-    `exercise_date` date                  NOT NULL,
-    `created_at`    datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`    datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
-);
-
-CREATE TABLE `exercise_detail` (
-    `id`                 bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `exercise_record_id` bigint                NOT NULL,
-    `exercise_id`        bigint                NOT NULL,
-    `set_detail_id`      bigint                NOT NULL,
-    `user_id`            bigint                NOT NULL,
-    `time`               int                   NULL,
-    `intensity`          ENUM('LOOSE', 'NORMAL', 'TIGHT') NULL,
-    `created_at`         datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`         datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`exercise_record_id`) REFERENCES `exercise_record`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`exercise_id`) REFERENCES `exercise`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`set_detail_id`) REFERENCES `set_detail`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
-);
-
-CREATE TABLE `exercise_routine` (
-    `id`           bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `user_id`      bigint                NOT NULL,
-    `name`         varchar(50)           NULL,
-    `created_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`   datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
-);
-
-CREATE TABLE `exercise_routine_information` (
-    `id`                  bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `exercise_routine_id` bigint                NOT NULL,
-    `exercise_id`         bigint                NOT NULL,
-    `created_at`          datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`          datetime              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`exercise_routine_id`) REFERENCES `exercise_routine`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`exercise_id`) REFERENCES `exercise`(`id`) ON DELETE CASCADE
 );
