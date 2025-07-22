f21ad8be6bbd6b8e607d72ba94b03f794e6a15cc
dlstkdals123 <dlstkdals123@naver.com>
2025-07-22 15:43:19 +0900

    [Feat] #41 - 식단 생성 API 구현

--- src/main/java/org/example/kuit_kac/domain/food/service/FoodService.java
+++ src/main/java/org/example/kuit_kac/domain/food/service/FoodService.java
@@ -3,8 +3,17 @@ package org.example.kuit_kac.domain.food.service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+import org.example.kuit_kac.domain.food.model.Food;
+import org.example.kuit_kac.domain.food.repository.FoodRepository;
 
 @Service
 @RequiredArgsConstructor
 public class FoodService {
+
+    private final FoodRepository foodRepository;
+
+    public Food getFoodById(Long id) {
+        return foodRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("음식 없음"));
+    }
 }
