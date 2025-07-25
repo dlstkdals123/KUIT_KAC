fbfcdd33932a2cda3846d2acf1065ab4dba2e330
임유리 <dladbfl67@naver.com>
2025-07-25 20:47:14 +0900

    /home/summary API 구현(테스트 전)

--- src/main/java/org/example/kuit_kac/domain/home/dto/HomeSummaryResponse.java
+++ src/main/java/org/example/kuit_kac/domain/home/dto/HomeSummaryResponse.java
@@ -9,10 +9,10 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class HomeSummaryResponse {
-    // 오늘 남은 칼로리, 오늘 섭취한 칼로리, 목표일일칼로리, 현재 체중
+    // 일일섭취목표, 오늘 먹은 음식들의 칼로리, 현재 체중, 남은 칼로리
 
-    private double remainingKCalorie;
     private double dailyKCalorieGoal;
     private double totalIntakeKCalorie;
     private double weight;
+    private double remainingKCalorie;
 }
