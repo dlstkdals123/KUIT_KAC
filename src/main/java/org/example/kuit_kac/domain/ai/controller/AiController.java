b275de5947fe3e0ed734ce7b160d42aa7b98c626
dlstkdals123 <dlstkdals123@naver.com>
2025-08-18 03:20:26 +0900

    [Feat] #75 - GPT API 구현

--- src/main/java/org/example/kuit_kac/domain/ai/controller/AiController.java
+++ src/main/java/org/example/kuit_kac/domain/ai/controller/AiController.java
@@ -1,13 +1,21 @@
 package org.example.kuit_kac.domain.ai.controller;
 
+import java.nio.file.attribute.UserPrincipal;
+
+import org.example.kuit_kac.exception.CustomException;
+import org.example.kuit_kac.exception.ErrorCode;
 import org.example.kuit_kac.config.GptConfig;
+import org.example.kuit_kac.domain.ai.dto.AIPlanCreateRequest;
 import org.example.kuit_kac.domain.ai.service.AiDietService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,10 +28,18 @@ public class AiController {
 
     private final AiDietService aiDietService;
 
-    @GetMapping("/diets")
-    public ResponseEntity<String> getResponse() {
-        String systemPrompt = aiDietService.getSystemPrompt();
-        String userPrompt = aiDietService.getUserPrompt();
-        return gptConfig.getResponse(systemPrompt, userPrompt);
+    @PostMapping("/diets")
+    public ResponseEntity<String> getResponse(
+        @RequestBody @Valid AIPlanCreateRequest request,
+        @AuthenticationPrincipal UserPrincipal p
+    ) {
+        if (p == null) {
+            throw new CustomException(ErrorCode.AUTH_UNAUTHORIZED);
+        }
+        String systemPrompt = aiDietService.getSystemPrompt(request);
+        String userPrompt = aiDietService.getUserPrompt(request);
+        String dayResponse = gptConfig.getResponse(systemPrompt, userPrompt);
+        String response = aiDietService.convertDayToDate(dayResponse, request);
+        return ResponseEntity.ok(response);
     }
 }
