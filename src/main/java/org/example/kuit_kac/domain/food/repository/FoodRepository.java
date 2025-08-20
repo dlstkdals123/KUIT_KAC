e71137b8b73de87eafeee931e68e591b18abd900
dlstkdals123 <dlstkdals123@naver.com>
2025-08-21 04:12:04 +0900

    [Feat] #122 - aifood 관련 서비스 삭제

--- src/main/java/org/example/kuit_kac/domain/food/repository/FoodRepository.java
+++ src/main/java/org/example/kuit_kac/domain/food/repository/FoodRepository.java
@@ -1,6 +1,7 @@
 package org.example.kuit_kac.domain.food.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.example.kuit_kac.domain.food.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
+    Optional<Food> findByName(String name);
     List<Food> findByIdGreaterThanOrderByIdAsc(Long id);
 }
